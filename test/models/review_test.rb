e1f411f7cd0bbf24ca744c11247dc022c17f4871
qqdipps <sdipp007@gmail.com>
2019-05-01 23:00:34 -0700

    added validations for review model tests

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -4,20 +4,29 @@ describe Review do
   let(:review) { reviews(:review_1) }
 
   it "must be valid" do
-    expect(review.valid?).must_be true
+    expect(review.valid?).must_equal true
   end
 
   describe "validations" do
     it "will not be valid if missing rating" do
-      # review.rating = nil 
+      review.rating = nil
       expect(review.valid?).must_equal false
-    
+      expect(review.errors.include?(:rating)).must_equal true
+      expect(review.errors[:rating]).must_equal ["can't be blank", "is not included in the list"]
     end
 
     it "will not be valid if rating is less than 1" do
+      review.rating = -1
+      expect(review.valid?).must_equal false
+      expect(review.errors.include?(:rating)).must_equal true
+      expect(review.errors[:rating]).must_equal ["is not included in the list"]
     end
 
     it "will not be valid if rating is greater than 5" do
+      review.rating = 6
+      expect(review.valid?).must_equal false
+      expect(review.errors.include?(:rating)).must_equal true
+      expect(review.errors[:rating]).must_equal ["is not included in the list"]
     end
   end
 end
