a109277cf3aa658f4d8c96cd090a5465dc4c6f6f
qqdipps <sdipp007@gmail.com>
2019-05-01 20:59:52 -0700

    completed model for merchant

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -7,28 +7,41 @@ describe Merchant do
     expect(merchant.valid?).must_equal true
   end
 
-  # validates :username, presence: true, uniqueness: true
-  # validates :email, presence: true, uniqueness: true
-  # validates :uid, presence: true
-  # validates :provider, presence: true
-
   describe "validations" do
     it "will not be valid if missing username" do
+      merchant.username = nil
+      expect(merchant.valid?).must_equal false
     end
 
     it "will not be valid if username is not unique" do
+      merchant_dup = Merchant.new(username: merchant.username,
+                                  email: "duplicate@adda.com",
+                                  uid: "4527",
+                                  provider: "github")
+      expect(merchant_dup.valid?).must_equal false
     end
 
     it "will not be valid if missing email" do
+      merchant.email = nil
+      expect(merchant.valid?).must_equal false
     end
 
     it "will not be valid if email is not unique" do
+      merchant_dup = Merchant.new(username: "gilfoyle",
+                                  email: merchant.email,
+                                  uid: "4527",
+                                  provider: "github")
+      expect(merchant_dup.valid?).must_equal false
     end
 
     it "will not be valid if missing uid" do
+      merchant.uid = nil
+      expect(merchant.valid?).must_equal false
     end
-    
+
     it "will not be valid if missing provider" do
+      merchant.provider = nil
+      expect(merchant.valid?).must_equal false
     end
   end
 end
