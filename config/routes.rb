c37cca458e88bb2c5f620f21f1f60377acc3ac8b
Margaret Finnan <msfinnan@gmail.com>
2019-05-02 15:29:36 -0700

    basic show page and index for products

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   get "/auth/github", as: "github_login"
   get "/auth/:provider/callback", to: "users#create"
-  delete "/logout", to: "users#destroy", as: “logout”
+  delete "/logout", to: "users#destroy", as: "logout"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,34 +1,53 @@
 require "test_helper"
 
 describe ProductsController do
-  it "should get new" do
-    get products_new_url
-    value(response).must_be :success?
+  # it "should get new" do
+  #   get products_new_url
+  #   value(response).must_be :success?
+  # end
+
+  # it "should get create" do
+  #   get products_create_url
+  #   value(response).must_be :success?
+  # end
+
+  describe "index" do
+    it "should get index" do
+      # Act
+      get products_path
+
+      # Assert
+      must_respond_with :success
+    end
   end
 
-  it "should get create" do
-    get products_create_url
-    value(response).must_be :success?
+  describe "show" do
+    it "can get a valid product" do
+
+      # Act
+      get product_path(products(:product_1).id)
+
+      # Assert
+      must_respond_with :success
+    end
+
+    it "will redirect for an invalid product" do
+
+      # Act
+      get product_path(-1)
+
+      # Assert
+      must_respond_with :redirect
+    end
   end
 
-  it "should get index" do
-    get products_index_url
-    value(response).must_be :success?
-  end
+  # it "should get edit" do
+  #   get products_edit_url
+  #   value(response).must_be :success?
+  # end
 
-  it "should get show" do
-    get products_show_url
-    value(response).must_be :success?
-  end
-
-  it "should get edit" do
-    get products_edit_url
-    value(response).must_be :success?
-  end
-
-  it "should get update" do
-    get products_update_url
-    value(response).must_be :success?
-  end
-
+  # it "should get update" do
+  #   get products_update_url
+  #   value(response).must_be :success?
+  # end
 end
