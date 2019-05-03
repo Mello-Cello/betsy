df4ed7565235a3cb4a61cf47c9061d41df6676dd
qqdipps <sdipp007@gmail.com>
2019-05-03 12:16:33 -0700

    created items create action, need order to make valid item, moving to that then will return

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,19 @@ class ItemsController < ApplicationController
     @product = Product.find_by(id: params[:product_id])
     item = Item.new(item_params)
     if @product.stock <= item.quantity
+      flash.now[:error] = "Could Not Add To Cart: quantity selected is more than available stock"
       render "product/show", status: :bad_request
+    end
+    @product.items << item
+    if item.valid?
+      flash[:success] = "#{@product.name} Successfully Added To Cart"
+      redirect_to product_path(@product.id)
+    else
+      flash.now[:error] = "Could Not Add To Cart"
+      item.errors.messages.each do |label, message|
+        flash.now[label.to_sym] = message
+      end
+      render "products/show", status: :bad_request
     end
   end
 
