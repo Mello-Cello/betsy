48914569f376ff4710c16dd7d8ee806adb75c293
qqdipps <sdipp007@gmail.com>
2019-05-02 22:56:40 -0700

    added create controller for reviews, partial form, updated routes to nested

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,21 @@
 class ReviewsController < ApplicationController
   def create
+    @product = Product.find(params[:product_id])
+    review = Review.new(review_params)
+    @product.reviews << review
+    if review.valid?
+      flash[:success] = "Review Successfully Added"
+      redirect_to product_path(@product.id)
+    else
+      flash[:failure] = "Could Not Add Review"
+      flash[:messages] = review.errors.messages
+      render "products/show", status: :bad_request
+    end
+  end
+
+  private
+
+  def review_params
+    return params.require(:review).permit(:comment, :rating)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  resources :categories, only: [:new, :create, :index]
-  resources :products, except: [:delete]
-  resources :items, only: [:create, :update, :delete]
+  # resources :categories, only: [:new, :create, :index]
+  resources :products, except: [:delete] do
+    resources :reviews, only: [:create]
+  end
+  # resources :items, only: [:create, :update, :delete]
   resources :merchants, only: [:create, :show, :delete]
-  resources :reviews, only: [:create]
-  resources :orders # UPDATE THIS AFTER WE DECIDE WHAT WE NEED/DON'T
+
+  # resources :orders # UPDATE THIS AFTER WE DECIDE WHAT WE NEED/DON'T
 
   get "/auth/github", as: "github_login"
   get "/auth/:provider/callback", to: "users#create"
