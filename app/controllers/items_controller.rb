08423cba5939b67c9c1a32632a990bb60799934f
qqdipps <sdipp007@gmail.com>
2019-05-03 11:56:35 -0700

    setting up create controller, adding link to view, and nested routes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,21 @@
 class ItemsController < ApplicationController
   def create
+    @product = Product.find_by(id: params[:product_id])
+    item = Item.new(item_params)
+    if @product.stock <= item.quantity
+      render "product/show", status: :bad_request
+    end
   end
 
   def update
   end
 
   def delete
+  end
+
+  private
+
+  def item_params
+    return params.require(:item).permit(:quantity)
   end
 end
