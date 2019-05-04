f897f4f408abb7aa21e09818802377b64a9a1dc8
qqdipps <sdipp007@gmail.com>
2019-05-04 15:15:46 -0700

    switching tasks

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,20 +2,25 @@ class ItemsController < ApplicationController
   def create
     # find product and order
     @product = Product.find_by(id: params[:product_id])
-    order = Order.find_by(id: session[:order_id])
+    order = Order.find_by(id: session[:cart_id])
+    # if no current cart, create order and save to session.
+    unless order
+      order = Order.create
+      session[:cart_id] = order.id
+    end
 
-    item = Item.new(item_params)
+    item = order.items.find_by(product_id: @product.id)
+    if item
+      item.quantity += params[:item][:quantity]
+    else
+      item = Item.new(item_params)
+    end
 
     # check if requested quanitity is available
     if @product.stock < item.quantity
       flash.now[:error] = "Could Not Add To Cart: quantity selected is more than available stock"
       render "products/show", status: :bad_request
     else
-      # if no current order, create order and save to session.
-      unless order
-        order = Order.create
-        session[:order_id] = order.id
-      end
 
       # set up relationships for item
       @product.items << item
