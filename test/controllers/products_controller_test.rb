ee39379ecb5565bf2aa0d4d15509c712c15cfa10
Margaret Finnan <msfinnan@gmail.com>
2019-05-05 12:54:39 -0700

    added more products controller tests and fixed a view on the products form

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -53,38 +53,63 @@ describe ProductsController do
   describe "create" do
     describe "logged in user" do
       it "creates a product with valid data" do
-        merchant = merchants(:merchant_1)
+        merchant = perform_login
         new_product = {product: {name: "Something amazing", merchant_id: merchant.id, price: 1000}}
         expect {
           post products_path, params: new_product
         }.must_change "Product.count", 1
 
-        # new_product_id = Product.find_by(name: "Something amazing").id
+        new_product_id = Product.find_by(name: "Something amazing").id
 
-        # must_respond_with :redirect
-        # must_redirect_to work_path(new_work_id)
+        must_respond_with :redirect
+        must_redirect_to product_path(new_product_id)
       end
     end
 
-    # it "renders bad_request and does not update the DB for bogus data" do
-    #   bad_work = {work: {title: nil, category: "book"}}
+    it "renders bad_request and does not update the DB for bogus data" do
+      merchant = perform_login
+      bad_prod_name = {product: {name: nil, merchant_id: merchant.id, price: 1000}}
+      bad_merch_id = {product: {name: "Awesome product", merchant_id: nil, price: 1000}}
+      bad_price = {product: {name: "Awesomer product", merchant_id: merchant.id, price: 0}}
 
-    #   expect {
-    #     post works_path, params: bad_work
-    #   }.wont_change "Work.count"
+      expect {
+        post products_path, params: bad_prod_name
+      }.wont_change "Product.count"
 
-    #   must_respond_with :bad_request
-    # end
+      must_respond_with :bad_request
+      expect(flash[:error]).must_include "Could not add new product"
+      expect(flash[:error]).must_include "name"
+      expect(flash[:error]).must_include "can't be blank"
 
-    # it "renders 400 bad_request for bogus categories" do
-    #   INVALID_CATEGORIES.each do |category|
-    #     invalid_work = {work: {title: "Invalid Work", category: category}}
+      expect {
+        post products_path, params: bad_merch_id
+      }.wont_change "Product.count"
 
-    #     proc { post works_path, params: invalid_work }.wont_change "Work.count"
+      must_respond_with :bad_request
+      expect(flash[:error]).must_include "Could not add new product"
+      expect(flash[:error]).must_include "merchant"
+      expect(flash[:error]).must_include "must exist"
 
-    #     Work.find_by(title: "Invalid Work", category: category).must_be_nil
-    #     must_respond_with :bad_request
-    #   end
-    # end
+      expect {
+        post products_path, params: bad_price
+      }.wont_change "Product.count"
+
+      must_respond_with :bad_request
+      expect(flash[:error]).must_include "Could not add new product"
+      expect(flash[:error]).must_include "must be greater than 0"
+    end
+  end
+
+  describe "logged out user" do
+    it "cannot create a product with valid data" do
+      # is setting merchant id to nil enough to test this?
+      new_product = {product: {name: "Something amazing", merchant_id: nil, price: 1000}}
+      expect {
+        post products_path, params: new_product
+      }.wont_change "Product.count"
+
+      must_respond_with :bad_request
+      expect(flash[:error]).must_equal "You must be logged in to create a new product"
+    end
   end
 end
