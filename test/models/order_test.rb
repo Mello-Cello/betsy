711f6626f510d988e0cc441d6a53d2c09a9df26f
qqdipps <sdipp007@gmail.com>
2019-05-06 14:35:49 -0700

    tested changes to schema for order validations on status

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -7,19 +7,38 @@ describe Order do
     expect(order.valid?).must_equal true
   end
 
-# has_many :items
-describe "relationship with an order" do
-  it "can have 0 items" do
-    Item.destroy_all
+  describe "validations" do
+    it "has a default value of 'pending'" do
+      order = Order.new
+      expect(order.status).must_equal "pending"
+    end
+
+    it "will be valid if given status of [\"pending\", \"paid\", \"complete\", \"cancelled\"]" do
+      ["pending", "paid", "complete", "cancelled"].each do |status|
+        order.update(status: status)
+        expect(order.valid?).must_equal true
+      end
+    end
+
+    it "will not be valid if status is different from above" do
+      ["penddding", "paiad", "compledte", "cancnelled"].each do |status|
+        order.update(status: status)
+        expect(order.valid?).must_equal false
+      end
+    end
+  end
+  describe "relationship with an order" do
+    it "can have 0 items" do
+      Item.destroy_all
       expect(order.items.count).must_equal 0
+    end
+
+    it "can have 1 or more items" do
+      expect(order.items.count).must_equal 2
+    end
   end
 
-  it "can have 1 or more items" do
-    expect(order.items.count).must_equal 2
-  end
-end
-  
-it "can shovel items to create a relationship" do
+  it "can shovel items to create a relationship" do
     product = products(:product_1)
     item = Item.new(quantity: 3, product: product)
     expect(item.valid?).must_equal false
