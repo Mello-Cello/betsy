31935ffecd3d45f5e97d8495e116e31e6419a20e
Margaret Finnan <msfinnan@gmail.com>
2019-05-07 10:28:46 -0700

    added custom model method to make items hash

--- app/models/order.rb
+++ app/models/order.rb
@@ -27,4 +27,28 @@ class Order < ApplicationRecord
     end
     return true
   end
+
+  #self method for merchant dashboard
+  # def self.sort_work(category)
+  #   works = Work.where(category: category).to_a
+  #   works.sort_by! { |work| Vote.where(work_id: work.id).length }
+  #   return works.reverse
+  # end
+
+  def self.find_merchant_order_items(merchant)
+    items_hash = {
+      paid: [],
+      complete: [],
+    }
+    Orders.all.each do |order|
+      order.items.each do |item|
+        if order.status == "paid" && item.product.merchant_id == merchant.id
+          items_hash[:paid] << item
+        elsif order.status == "complete" && item.product.merchant_id == merchant.id
+          items_hash[:complete] << item
+        end
+      end
+      return items_hash
+    end
+  end
 end
